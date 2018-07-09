e9f293a77ce2ae441e36ca1ce0a679cdcad46dd5
Anis AYARI <anis.ayari.perso@gmail.com>
2018-07-09 11:46:00 +0200

    fix main script exampl

--- example/main.py
+++ example/main.py
@@ -8,15 +8,15 @@ stoppath = "Stoplist.txt"
 
 
 if __name__ == '__main__':
-    # title_theme_list = []
-    # title_theme_list.append(('Soccer', 'Soccer'))
-    # title_theme_list.append(('Baseball', 'Baseball'))
-    # title_theme_list.append(('Golf', 'Golf'))
-    # title_theme_list.append(('Basketball', 'Basketball'))
-    # title_theme_list.append(('Judo', 'Judo'))
-    #
-    # sm.ConstructWikiDico(wiki_dico_path, title_theme_list, init=True)
-    # sm.ConstructDatabaseFromKnwoledgebase(wiki_dico_path,database_file_ouput="database_file_custom_name.csv")
+    title_theme_list = []
+    title_theme_list.append(('Soccer', 'Soccer'))
+    title_theme_list.append(('Baseball', 'Baseball'))
+    title_theme_list.append(('Golf', 'Golf'))
+    title_theme_list.append(('Basketball', 'Basketball'))
+    title_theme_list.append(('Judo', 'Judo'))
+
+    sm.ConstructWikiDico(wiki_dico_path, title_theme_list, init=True)
+    sm.ConstructDatabaseFromKnwoledgebase(wiki_dico_path,database_file_ouput="database_file_custom_name.csv")
     
     df = sm.ImportDatabase(database_file = "database_file_custom_name.csv",stoppath=stoppath)
     
